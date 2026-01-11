e51cf5877bfadb2d581b60ba5b4cd7765f3f967f
mogoo <dnwnd501@gmail.com>
2026-01-11 15:54:42 +0900

    [Refactor] extract Header styles to separate file and fix stacking context
    - Move all styled-components from 'Header.tsx' to 'style.ts' for better
      readability
    - Add 'z-indx: 100' to 'Nav' to ensure header stays on top of content
    - Standardize exports and imports for Header layout components

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,4 +1,3 @@
-import styled from "styled-components";
 import {
   motion,
   stagger,
@@ -11,90 +10,17 @@ import { Link, useMatch, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useLayoutReady } from "../../../hooks/useLayoutReady";
 import { useForm } from "react-hook-form";
-
-const Nav = styled(motion.nav)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  position: fixed;
-  width: 100%;
-  top: 0;
-  background-color: ${(props) => props.theme.black.darker};
-  font-size: 14px;
-  padding: 20px 60px;
-  color: ${(props) => props.theme.white.lighter};
-`;
-
-const Col = styled.div`
-  display: flex;
-  align-items: center;
-`;
-
-const Logo = styled(motion.svg)`
-  margin-right: 50px;
-  width: 95px;
-  height: 25px;
-  fill: ${(props) => props.theme.red};
-  path {
-    stroke: ${(props) => props.theme.red};
-    stroke-width: 3px;
-    stroke-miterlimit: 10;
-  }
-`;
-
-const Items = styled.ul`
-  display: flex;
-  align-items: center;
-  min-height: 40px;
-`;
-
-const Item = styled.li`
-  margin-right: 20px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  flex-direction: column;
-  position: relative;
-`;
-
-const ItemBottomBar = styled(motion.div)`
-  width: 100%;
-  height: 3px;
-  border-radius: 2px;
-  background-color: ${(props) => props.theme.red};
-  position: absolute;
-  bottom: -5px;
-`;
-
-const SearchForm = styled.form`
-  color: ${(props) => props.theme.white.lighter};
-  display: flex;
-  align-items: center;
-  position: relative;
-`;
-
-const SearchSVG = styled(motion.svg)`
-  height: 25px;
-  fill: ${(props) => props.theme.white.lighter};
-  cursor: pointer;
-`;
-
-const SearchInput = styled(motion.input)`
-  position: absolute;
-  right: 0px;
-  z-index: -1;
-  height: 40px;
-  border: 1px solid ${(props) => props.theme.white.lighter};
-  outline: none;
-  border-radius: 5px;
-  transform-origin: right center;
-  min-width: 230px;
-  padding: 5px 10px;
-  padding-left: 40px;
-  font-size: 16px;
-  color: ${(props) => props.theme.white.lighter};
-  background-color: transparent;
-`;
+import {
+  Col,
+  Item,
+  ItemBottomBar,
+  Items,
+  Logo,
+  Nav,
+  SearchForm,
+  SearchInput,
+  SearchSVG,
+} from "./style";
 
 const logoPaths = [
   "M10,100 L10,0 L35,0 L75,65 L75,0 L100,0 L100,96 L75,96 L35,30 L35,98 Z", // N
---
+++ src/components/layout/Header/style.ts
@@ -0,0 +1,87 @@
+import { motion } from "motion/react";
+import styled from "styled-components";
+
+export const Nav = styled(motion.nav)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  position: fixed;
+  width: 100%;
+  top: 0;
+  background-color: ${(props) => props.theme.black.darker};
+  font-size: 14px;
+  padding: 20px 60px;
+  color: ${(props) => props.theme.white.lighter};
+  z-index: 100;
+`;
+
+export const Col = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Logo = styled(motion.svg)`
+  margin-right: 50px;
+  width: 95px;
+  height: 25px;
+  fill: ${(props) => props.theme.red};
+  path {
+    stroke: ${(props) => props.theme.red};
+    stroke-width: 3px;
+    stroke-miterlimit: 10;
+  }
+`;
+
+export const Items = styled.ul`
+  display: flex;
+  align-items: center;
+  min-height: 40px;
+`;
+
+export const Item = styled.li`
+  margin-right: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  position: relative;
+`;
+
+export const ItemBottomBar = styled(motion.div)`
+  width: 100%;
+  height: 3px;
+  border-radius: 2px;
+  background-color: ${(props) => props.theme.red};
+  position: absolute;
+  bottom: -5px;
+`;
+
+export const SearchForm = styled.form`
+  color: ${(props) => props.theme.white.lighter};
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+export const SearchSVG = styled(motion.svg)`
+  height: 25px;
+  fill: ${(props) => props.theme.white.lighter};
+  cursor: pointer;
+`;
+
+export const SearchInput = styled(motion.input)`
+  position: absolute;
+  right: 0px;
+  z-index: -1;
+  height: 40px;
+  border: 1px solid ${(props) => props.theme.white.lighter};
+  outline: none;
+  border-radius: 5px;
+  transform-origin: right center;
+  min-width: 230px;
+  padding: 5px 10px;
+  padding-left: 40px;
+  font-size: 16px;
+  color: ${(props) => props.theme.white.lighter};
+  background-color: transparent;
+`;
